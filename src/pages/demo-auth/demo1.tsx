c08ff4fc85bfef7161c68a0fc010eb011c635865
Ky Pham <kypv@i.lisod.vn>
2023-12-13 21:30:16 +0700

    update

---
+++ src/pages/demo-auth/demo1.tsx
@@ -0,0 +1,9 @@
+export default function demo1() {
+  //   useEffect(() => {
+  //     localStorage.setItem('myData', 'Hello from Page A');
+  //     setTimeout(() => {
+  //       window.close();
+  //     }, 300);
+  //   }, []);
+  return <div>demo1</div>;
+}
