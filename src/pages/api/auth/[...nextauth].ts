198034cd3d1d421b57b098162db6af93bce76ca4
hailv <bm.hcmc.duytruong@modetour.com>
2024-01-09 09:36:55 +0700

    test_login_tiktok

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,7 +3,6 @@
 import NextAuth from 'next-auth';
 import TwitterProvider from 'next-auth/providers/twitter';
 import DiscordProvider from 'next-auth/providers/discord';
-import TikTokProvider from 'next-auth/providers/tiktok';
 import { discordProvider } from '@/utils/social-provider-configs/discord.provider';
 import LineProvider from 'next-auth/providers/line';
 import type { NextAuthOptions } from 'next-auth';
