1405acebe56038207b82c8a99aa0fc2475fab2e6
Ky Pham <kypv@i.lisod.vn>
2024-01-17 11:03:35 +0700

    update

--- src/components/CampaignRewardCardItem/index.tsx
+++ src/components/CampaignRewardCardItem/index.tsx
@@ -1,10 +1,12 @@
+import { TypeCampaignReward } from '@/redux/endpoints/campaign';
+import Image from 'next/image';
 import { useMemo, useState } from 'react';
 import CShadowCard from '../common/CCardShadow';
 import ArrowDown from '../common/icons/ArrowDown';
 
-export default function CampaignRewardCardItem() {
+export default function CampaignRewardCardItem({ campaignReward }: { campaignReward: TypeCampaignReward }) {
   const [isViewMoreRewardMethod, setIsViewMoreRewardMethod] = useState(false);
-  const rewardMethod = Array(20).fill('item');
+  const rewardMethod = Array(campaignReward?.numberOfWinningTicket ?? 0).fill('item');
   const renderRewardItem = useMemo(() => {
     if (!Array.isArray(rewardMethod)) {
       return [];
@@ -17,16 +19,17 @@ export default function CampaignRewardCardItem() {
     }
     return rewardMethod.slice(0, 3);
   }, [rewardMethod, isViewMoreRewardMethod]);
+
   return (
-    <CShadowCard>
+    <CShadowCard disableClick>
       <div className="font-notoSans p-[24px] flex flex-col gap-[16px]   ">
         <div className="flex gap-[16px] items-center justify-center font-bold">
           <span>
-            <span className="text-[24px] font-montserrat">1</span>
+            <span className="text-[24px] font-montserrat">{campaignReward?.index ?? '--'}</span>
             <span className=" text-[20px] ">等</span>
           </span>
           <span>
-            <span className="text-[34px] font-montserrat">10,000</span>
+            <span className="text-[34px] font-montserrat">{campaignReward?.amountOfMoney ?? '--'}</span>
             <span className=" text-[24px] ">円</span>
           </span>
         </div>
@@ -34,7 +37,16 @@ export default function CampaignRewardCardItem() {
           <div className="flex flex-wrap justify-between items-end gap-[12px]  transition-all duration-150">
             <div className="flex flex-wrap gap-[8px]">
               {renderRewardItem.map(() => (
-                <div className="w-[32px] h-[32px] min-w-[32px] min-h-[32px] rounded-full bg-gray-1" />
+                <div className="w-[32px] h-[32px] min-w-[32px] min-h-[32px] rounded-full  ">
+                  <Image
+                    alt="campaign image"
+                    className="w-full h-full object-contain"
+                    height="0"
+                    sizes="100vw"
+                    src="/assets/images/amazon-reward-card.png"
+                    width="0"
+                  />
+                </div>
               ))}
             </div>
             {rewardMethod?.length > 3 ? (
