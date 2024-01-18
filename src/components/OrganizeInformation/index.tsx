f2ddc47ad1621e0641e6b7e40c7574ccc3e6bcd6
hailv <bm.hcmc.duytruong@modetour.com>
2024-01-18 13:01:24 +0700

    update_user

--- src/components/OrganizeInformation/index.tsx
+++ src/components/OrganizeInformation/index.tsx
@@ -1,115 +1,186 @@
-import { Form, Image } from 'antd';
 import React, { useState } from 'react';
+import { Form, Image, Spin } from 'antd';
 import { usePopUpContext } from '@/context/PopUpContext';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '@/redux/store';
-import BasicButton from '../common/BasicButton';
+import { TypeTokenPayment } from '@/types/paymentCard.type';
+import { CompaniesParams, useUpdateCompaniesMutation } from '@/redux/endpoints/companies';
+import { useLazyMeQuery } from '@/redux/endpoints/auth';
+import toastMessage from '@/utils/func/toastMessage';
+import { setUser } from '@/redux/slices/auth.slice';
 import BasicInput from '../common/BasicInput';
 import UploadButton from '../common/UploadButton';
 import PopUpCreditOrDebitCard from './PopUpCreditOrDebitCard';
 import CButtonShadow from '../common/CButtonShadow';
+import CButtonClassic from '../common/CButtonClassic';
 
 function OrganizeInformation() {
-  const [isEdit, setIsEdit] = useState<boolean>(false);
+  const [form] = Form.useForm();
   const { openPopUp } = usePopUpContext();
   const { user } = useSelector((state: RootState) => state.auth);
-  // const initialValue = useMemo(()=>({
-
-  // }),[])
+  const [isEdit, setIsEdit] = useState<boolean>(false);
+  const [paymentMethod, setPaymentMethod] = useState<TypeTokenPayment | undefined>(undefined);
+  const [trigger, { isLoading: loadingUpdate }] = useUpdateCompaniesMutation();
+  const [triggerMe] = useLazyMeQuery();
+  const dispatch = useDispatch();
 
   return (
-    <Form className="px-[48px] pb-[100px]">
-      <div className="border-b-2 border-[#2D3648]  py-[24px]">
-        <span className="text-[32px] font-bold">組織情報</span>
-      </div>
-      <div className="bg-white rounded-[16px] p-[48px] mt-[56px]">
-        <div className="flex flex-col space-y-[32px]">
-          <div className="flex flex-col space-y-[16px]">
-            <span className="text-[18px] font-bold text-[#04AFAF] leading-[16px]">組織名</span>
-            {isEdit ? (
-              <Form.Item initialValue={user?.memberCompany?.name} name="name">
-                <BasicInput />
-              </Form.Item>
-            ) : (
-              <span className="text-[16px] leading-[24px]">{user?.memberCompany?.name}</span>
-            )}
-          </div>
-          <div className="flex flex-col space-y-[16px]">
-            <span className="text-[18px] font-bold text-[#04AFAF] leading-[16px]">組織ID</span>
-            <span className="text-[16px] leading-[24px]">{user?.memberCompany?.code}</span>
-          </div>
-          <div className="flex flex-col space-y-[16px]">
-            <span className="text-[18px] font-bold text-[#04AFAF] leading-[16px]">組織 代表メールアドレス </span>
-            {isEdit ? (
-              <Form.Item initialValue={user?.memberCompany?.email?.email} name="email">
-                <BasicInput />
-              </Form.Item>
-            ) : (
-              <span className="text-[16px] leading-[24px]">{user?.memberCompany?.email?.email}</span>
-            )}
-          </div>
-          <div className="flex flex-col">
-            <span className="text-[18px] font-bold text-[#04AFAF] leading-[16px]">ロゴ</span>
-            {isEdit ? (
-              <div className="mt-[12px]">
-                <UploadButton className="w-[175px]" />
-              </div>
-            ) : (
-              <div className="mt-[12px] w-[189px] h-[56px] ">
-                <Image
-                  alt="image"
-                  className="object-cover"
-                  crossOrigin="anonymous"
-                  height="56px"
-                  preview={false}
-                  src={user?.memberCompany?.image?.imageUrl}
-                  width="189px"
-                />
-              </div>
-            )}
-          </div>
-          <div className="flex flex-col space-y-[16px] pb-[24px]">
-            <span className="text-[18px] font-bold text-[#04AFAF] leading-[16px]">支払方法</span>
-            <div className="flex flex-col text-[16px] leading-[24px] space-y-[20px]">
-              <span>{user?.memberCompany?.cardInfo?.cardBrand}</span>
-              <span>末尾が••••{user?.memberCompany?.cardInfo?.lastFour}のクレジットカード</span>
+    <Spin spinning={loadingUpdate}>
+      <Form
+        className="px-[48px] pb-[100px]"
+        form={form}
+        onFinish={(e) => {
+          const obj: CompaniesParams = {
+            companyId: String(user?.companyId),
+            name: e.name,
+            email: e.email,
+            companyImage: e.companyImage,
+            cardInfo:
+              paymentMethod &&
+              JSON.stringify({
+                cardholderName: paymentMethod?.cardholderName,
+                lastFour: paymentMethod?.details?.card.last4,
+                cardBrand: paymentMethod?.details?.card.brand,
+              }),
+            sourceId: paymentMethod?.token,
+          };
+          Object.keys(obj).forEach((key) => {
+            if (obj[key] === null || obj[key] === undefined) {
+              delete obj[key];
+            }
+          });
+          trigger(obj)
+            .unwrap()
+            .then(() => {
+              triggerMe()
+                .unwrap()
+                .then((res) => {
+                  setIsEdit(false);
+                  dispatch(setUser(res));
+                });
+
+              toastMessage('update success', 'success');
+            })
+            .catch(() => toastMessage('error', 'error'));
+        }}
+      >
+        <div className="border-b-2 border-[#2D3648]  py-[24px]">
+          <span className="text-[32px] font-bold">組織情報</span>
+        </div>
+        <div className="bg-white rounded-[16px] p-[48px] mt-[56px]">
+          <div className="flex flex-col space-y-[32px]">
+            <div className="flex flex-col space-y-[16px]">
+              <span className="text-[18px] font-bold text-[#04AFAF] leading-[16px]">組織名</span>
+              {isEdit ? (
+                <Form.Item initialValue={user?.memberCompany?.name} name="name">
+                  <BasicInput />
+                </Form.Item>
+              ) : (
+                <span className="text-[16px] leading-[24px]">{user?.memberCompany?.name}</span>
+              )}
             </div>
-            {isEdit && (
-              <span
-                className="text-[18px] font-bold text-[#04AFAF]] cursor-pointer "
-                onClick={() => openPopUp({ contents: <PopUpCreditOrDebitCard /> })}
-                onKeyPress={undefined}
-                role="button"
-                tabIndex={0}
-              >
-                編集
-              </span>
-            )}
+            <div className="flex flex-col space-y-[16px]">
+              <span className="text-[18px] font-bold text-[#04AFAF] leading-[16px]">組織ID</span>
+              <span className="text-[16px] leading-[24px]">{user?.memberCompany?.code}</span>
+            </div>
+            <div className="flex flex-col space-y-[16px]">
+              <span className="text-[18px] font-bold text-[#04AFAF] leading-[16px]">組織 代表メールアドレス </span>
+              {isEdit ? (
+                <Form.Item initialValue={user?.memberCompany?.email?.email} name="email">
+                  <BasicInput />
+                </Form.Item>
+              ) : (
+                <span className="text-[16px] leading-[24px]">{user?.memberCompany?.email?.email}</span>
+              )}
+            </div>
+            <div className="flex flex-col">
+              <span className="text-[18px] font-bold text-[#04AFAF] leading-[16px]">ロゴ</span>
+              {isEdit ? (
+                <div className="mt-[12px]">
+                  <Form.Item name="companyImage" noStyle>
+                    <UploadButton className="w-[175px]" />
+                  </Form.Item>
+                </div>
+              ) : (
+                <div className="mt-[12px] w-[189px] h-[56px] ">
+                  <Image
+                    alt="image"
+                    className="object-cover"
+                    crossOrigin="anonymous"
+                    height="56px"
+                    preview={false}
+                    src={user?.memberCompany?.image?.imageUrl}
+                    width="189px"
+                  />
+                </div>
+              )}
+            </div>
+            <div className="flex flex-col space-y-[16px] pb-[24px]">
+              <span className="text-[18px] font-bold text-[#04AFAF] leading-[16px]">支払方法</span>
+              {!isEdit ? (
+                <div className="flex flex-col text-[16px] leading-[24px] space-y-[20px]">
+                  <span>{user?.memberCompany?.cardInfo?.cardBrand}</span>
+                  <span>末尾が••••{user?.memberCompany?.cardInfo?.lastFour}のクレジットカード</span>
+                </div>
+              ) : (
+                <div className="flex flex-col text-[16px] leading-[24px] space-y-[20px]">
+                  <span>
+                    {paymentMethod ? paymentMethod.details.card.brand : user?.memberCompany?.cardInfo?.cardBrand}
+                  </span>
+                  <span>
+                    末尾が••••
+                    {paymentMethod ? paymentMethod.details.card.last4 : user?.memberCompany?.cardInfo?.lastFour}
+                    のクレジットカード
+                  </span>
+                </div>
+              )}
+              {isEdit && (
+                <CButtonClassic
+                  customClassName="!rounded-[6px] !w-[90px] !h-[47px] !text-[14px] "
+                  onClick={() =>
+                    openPopUp({
+                      contents: <PopUpCreditOrDebitCard getCardPayment={(value) => setPaymentMethod(value)} />,
+                    })
+                  }
+                  title="編集"
+                />
+              )}
+            </div>
           </div>
         </div>
-      </div>
-      <div className=" pt-[48px]  h-full flex space-x-[24px] items-center justify-center">
-        {isEdit && (
-          <BasicButton onClick={() => setIsEdit(false)} type="primary">
-            キャンセル
-          </BasicButton>
-        )}
-        <div className="w-[146px]  h-[56px]">
-          <CButtonShadow
-            classBgColor="bg-white"
-            classRounded="rounded-[6px]"
-            classShadowColor="bg-main-text"
-            onClick={() => setIsEdit(true)}
-            shadowSize="normal"
-            textClass="bg-main-text"
-            title="編集する"
-          />
+        <div className=" pt-[48px]  h-full flex space-x-[24px] items-center justify-center">
+          {isEdit && (
+            <div className="w-[146px]  h-[56px]">
+              <CButtonShadow
+                classBgColor="bg-main-text"
+                classRounded="rounded-[6px]"
+                classShadowColor="bg-white"
+                onClick={() => setIsEdit(false)}
+                shadowSize="normal"
+                title="キャンセル"
+              />
+            </div>
+          )}
+          <div className="w-[146px]  h-[56px]">
+            <CButtonShadow
+              classBgColor="bg-white"
+              classRounded="rounded-[6px]"
+              classShadowColor="bg-main-text"
+              onClick={() => {
+                if (!isEdit) {
+                  setIsEdit(true);
+                } else {
+                  form.submit();
+                }
+              }}
+              shadowSize="normal"
+              textClass="bg-main-text"
+              title={isEdit ? '保存' : '編集する'}
+            />
+          </div>
         </div>
-        {/* <BasicButton className="h-[56px] w-[84px] " onClick={() => setIsEdit(true)}>
-          編集
-        </BasicButton> */}
-      </div>
-    </Form>
+      </Form>
+    </Spin>
   );
 }
 
--- src/components/PopUpOrganization/index.tsx
+++ src/components/PopUpOrganization/index.tsx
@@ -1,7 +1,7 @@
 import { usePopUpContext } from '@/context/PopUpContext';
 import React, { useEffect, useState } from 'react';
 // import { useRouter } from 'next/router';
-import { Form } from 'antd';
+import { Form, Spin } from 'antd';
 import { usePostCompaniesMutation } from '@/redux/endpoints/companies';
 import toastMessage from '@/utils/func/toastMessage';
 import { useLazyMeQuery } from '@/redux/endpoints/auth';
@@ -20,7 +20,7 @@ function PopUpOrganization() {
   const { openPopUp, closePopUp } = usePopUpContext();
   // const router = useRouter();
   const [form] = Form.useForm();
-  const [trigger] = usePostCompaniesMutation();
+  const [trigger, { isLoading }] = usePostCompaniesMutation();
   const [getMe] = useLazyMeQuery();
   const dispatch = useDispatch();
   const [paymentMethod, setPaymentMethod] = useState<TypeTokenPayment | undefined>(undefined);
@@ -47,146 +47,150 @@ function PopUpOrganization() {
   }, [paymentMethod]);
 
   return (
-    <div className="p-[64px] w-[928px] max-h-[829px] overflow-y-auto">
-      <Form
-        form={form}
-        onFinish={(e) => {
-          trigger({ ...e })
-            .unwrap()
-            .then(() => {
-              getMe()
-                .unwrap()
-                .then((res) => dispatch(setUser(res)));
-              toastMessage('success', 'success');
-              closePopUp();
-            })
-            .catch((err) => {
-              toastMessage(err.data.message, 'error');
-            });
-        }}
-        onFinishFailed={(e) => {
-          setErrorValidate(e.errorFields.map((v) => String(v.name[0])));
-        }}
-      >
-        <InputLabel
-          label="組織名"
-          name="name"
-          placeholder="記入してください"
-          required
-          rules={[
-            {
-              required: true,
-              message: 'ご入力いただいた組織名は既に使用されています。',
-            },
-          ]}
-        />
-        <InputLabel
-          label="組織ID"
-          name="code"
-          placeholder="選択してください"
-          required
-          rules={[
-            { required: true, message: 'ご入力いただいた組織IDは既に使用されています。' },
-            // {
-            //   validator() {
-            //     if (error?.data?.error === 'Conflict') {
-            //       // eslint-disable-next-line prefer-promise-reject-errors
-            //       return Promise.reject('ご入力いただいた組織名は既に使用されています。');
-            //     }
-            //     return Promise.resolve(' ');
-            //   },
-            // },
-          ]}
-        />
-        <InputLabel
-          label="組織 代表メールアドレス"
-          name="email"
-          placeholder="記入してください"
-          required
-          rules={[{ required: true, message: 'ご入力いただいた組織 代表メールアドレスは既に使用されています。' }]}
-        />
-
-        <div className="flex flex-col space-y-[10px] mb-[24px]">
-          <div className="flex space-x-[8px] items-center ">
-            <div className="text-[14px] font-bold ">ロゴ</div>
-            <div className="w-[35px] bg-[#04AFAF] h-[20px] text-center text-[11px] font-medium leading-[1.7] text-white rounded-[2px]">
-              必須
-            </div>
-          </div>
-          {/* <span className="font-bold">ロゴ </span> */}
-          <Form.Item name="companyImage" noStyle rules={[{ required: true, message: '' }]}>
-            <UploadButton
-              className={`w-[149px]  ${
-                errorValidate.includes('companyImage') && 'border-[#ff0000] border-2 rounded-[8px]'
-              } `}
-            />
-          </Form.Item>
-        </div>
-        <div className="flex flex-col space-y-[10px]">
-          <div className="flex space-x-[8px] items-center ">
-            <div className="text-[14px] font-bold ">お支払い方法</div>
-            <div className="w-[35px] bg-[#04AFAF] h-[20px] text-center text-[11px] font-medium leading-[1.7] text-white rounded-[2px]">
-              必須
-            </div>
-          </div>
-          {/* <span className="font-bold">お支払い方法</span> */}
-          <div className="hidden">
-            <Form.Item name="cardInfo" noStyle rules={[{ required: true, message: '' }]}>
-              <FlagItem />
-            </Form.Item>
-            <Form.Item name="sourceId" noStyle rules={[{ required: true, message: '' }]}>
-              <FlagItem />
-            </Form.Item>
-          </div>
-          {paymentMethod ? (
-            <div className="text-[16px] ">
-              {paymentMethod.details.card.brand} <br />
-              末尾が•••• {paymentMethod.details.card.last4}のクレジットカード
-            </div>
-          ) : (
-            <CButtonClassic
-              customClassName={`!rounded-[6px] !w-[290px] !h-[47px] !text-[14px] ${
-                errorValidate.includes('cardInfor' || 'sourceId') && 'border-[#ff0000]'
-              }`}
-              onClick={() =>
-                openPopUp({ contents: <PopUpCreditOrDebitCard getCardPayment={(value) => setPaymentMethod(value)} /> })
-              }
-              title="クレジットまたはデビットカードを追加"
-            />
-          )}
-        </div>
-        <div className="flex space-y-[16px] items-center flex-col border-t border-[#AAA] mt-[32px] pt-[40px]">
-          <div className="w-[195px]  h-[56px]">
-            <CButtonShadow
-              classBgColor="bg-main-text"
-              classRounded="rounded-[6px]"
-              classShadowColor="bg-white"
-              onClick={() => {
-                // router.push('/campaign/list');
-                // closePopUp();
-                form.submit();
-              }}
-              shadowSize="normal"
-              title="組織を作成する"
-            />
-          </div>
-
-          <span
-            className="text-[12px] font-medium underline underline-offset-2 cursor-pointer w-max text-[#333]"
-            onClick={() =>
-              openPopUp({
-                contents: <SearchoOganizationID />,
+    <Spin spinning={isLoading}>
+      <div className="p-[64px] w-[928px] max-h-[829px] overflow-y-auto">
+        <Form
+          form={form}
+          onFinish={(e) => {
+            trigger({ ...e })
+              .unwrap()
+              .then(() => {
+                getMe()
+                  .unwrap()
+                  .then((res) => dispatch(setUser(res)));
+                toastMessage('success', 'success');
+                closePopUp();
               })
-            }
-            onKeyPress={undefined}
-            role="button"
-            tabIndex={0}
-          >
-            既に存在する組織にサインインする
-          </span>
-        </div>
-      </Form>
-    </div>
+              .catch((err) => {
+                toastMessage(err.data.message, 'error');
+              });
+          }}
+          onFinishFailed={(e) => {
+            setErrorValidate(e.errorFields.map((v) => String(v.name[0])));
+          }}
+        >
+          <InputLabel
+            label="組織名"
+            name="name"
+            placeholder="記入してください"
+            required
+            rules={[
+              {
+                required: true,
+                message: 'ご入力いただいた組織名は既に使用されています。',
+              },
+            ]}
+          />
+          <InputLabel
+            label="組織ID"
+            name="code"
+            placeholder="選択してください"
+            required
+            rules={[
+              { required: true, message: 'ご入力いただいた組織IDは既に使用されています。' },
+              // {
+              //   validator() {
+              //     if (error?.data?.error === 'Conflict') {
+              //       // eslint-disable-next-line prefer-promise-reject-errors
+              //       return Promise.reject('ご入力いただいた組織名は既に使用されています。');
+              //     }
+              //     return Promise.resolve(' ');
+              //   },
+              // },
+            ]}
+          />
+          <InputLabel
+            label="組織 代表メールアドレス"
+            name="email"
+            placeholder="記入してください"
+            required
+            rules={[{ required: true, message: 'ご入力いただいた組織 代表メールアドレスは既に使用されています。' }]}
+          />
+
+          <div className="flex flex-col space-y-[10px] mb-[24px]">
+            <div className="flex space-x-[8px] items-center ">
+              <div className="text-[14px] font-bold ">ロゴ</div>
+              <div className="w-[35px] bg-[#04AFAF] h-[20px] text-center text-[11px] font-medium leading-[1.7] text-white rounded-[2px]">
+                必須
+              </div>
+            </div>
+            {/* <span className="font-bold">ロゴ </span> */}
+            <Form.Item name="companyImage" noStyle rules={[{ required: true, message: '' }]}>
+              <UploadButton
+                className={`w-[149px]  ${
+                  errorValidate.includes('companyImage') && 'border-[#ff0000] border-2 rounded-[8px]'
+                } `}
+              />
+            </Form.Item>
+          </div>
+          <div className="flex flex-col space-y-[10px]">
+            <div className="flex space-x-[8px] items-center ">
+              <div className="text-[14px] font-bold ">お支払い方法</div>
+              <div className="w-[35px] bg-[#04AFAF] h-[20px] text-center text-[11px] font-medium leading-[1.7] text-white rounded-[2px]">
+                必須
+              </div>
+            </div>
+            {/* <span className="font-bold">お支払い方法</span> */}
+            <div className="hidden">
+              <Form.Item name="cardInfo" noStyle rules={[{ required: true, message: '' }]}>
+                <FlagItem />
+              </Form.Item>
+              <Form.Item name="sourceId" noStyle rules={[{ required: true, message: '' }]}>
+                <FlagItem />
+              </Form.Item>
+            </div>
+            {paymentMethod ? (
+              <div className="text-[16px] ">
+                {paymentMethod.details.card.brand} <br />
+                末尾が•••• {paymentMethod.details.card.last4}のクレジットカード
+              </div>
+            ) : (
+              <CButtonClassic
+                customClassName={`!rounded-[6px] !w-[290px] !h-[47px] !text-[14px] ${
+                  errorValidate.includes('cardInfor' || 'sourceId') && 'border-[#ff0000]'
+                }`}
+                onClick={() =>
+                  openPopUp({
+                    contents: <PopUpCreditOrDebitCard getCardPayment={(value) => setPaymentMethod(value)} />,
+                  })
+                }
+                title="クレジットまたはデビットカードを追加"
+              />
+            )}
+          </div>
+          <div className="flex space-y-[16px] items-center flex-col border-t border-[#AAA] mt-[32px] pt-[40px]">
+            <div className="w-[195px]  h-[56px]">
+              <CButtonShadow
+                classBgColor="bg-main-text"
+                classRounded="rounded-[6px]"
+                classShadowColor="bg-white"
+                onClick={() => {
+                  // router.push('/campaign/list');
+                  // closePopUp();
+                  form.submit();
+                }}
+                shadowSize="normal"
+                title="組織を作成する"
+              />
+            </div>
+
+            <span
+              className="text-[12px] font-medium underline underline-offset-2 cursor-pointer w-max text-[#333]"
+              onClick={() =>
+                openPopUp({
+                  contents: <SearchoOganizationID />,
+                })
+              }
+              onKeyPress={undefined}
+              role="button"
+              tabIndex={0}
+            >
+              既に存在する組織にサインインする
+            </span>
+          </div>
+        </Form>
+      </div>
+    </Spin>
   );
 }
 
